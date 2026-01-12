eed8d2c100053c4bc5018c478537c3de417cfeb5
santonio-dockery <dockery0605@gmail.com>
2026-01-12 18:59:53 +0000

    Started in M1T1

---
+++ M1T1.cpp
@@ -0,0 +1,12 @@
+// CSC 154
+// M1T1 Module 1, tutorial 1
+
+//well explain later
+#include <iostream>
+using namespace std;
+
+int main() {        
+    cout << "Hello, World!" << endl;
+    cout << endl; //blank line
+  return 0; // exit with no errors
+}
