991ffdf95651ebde2f4cea74911172b51e47dea2
santonio-dockery <dockery0605@gmail.com>
2026-01-21 19:43:58 +0000

    started m1h1

---
+++ m1h1_dockery.cpp
@@ -0,0 +1,18 @@
+// CSC-134
+//m1h1
+//Santonio
+//1/21/26
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    string item_name = "Percy Jackson";
+    int item_count = 2010;
+
+    cout << "The Movie " << item_name << endl;
+    cout << "came out in " << item_count << endl;
+    
+
+}
