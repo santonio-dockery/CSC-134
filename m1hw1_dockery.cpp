da5bc9253fca27f77bf22bee7f4d5dacaa0f3326
santonio-dockery <dockery0605@gmail.com>
2026-01-26 19:17:13 +0000

    made my first line

--- m1hw1_dockery.cpp
+++ m1hw1_dockery.cpp
@@ -2,3 +2,17 @@
 //M1HW1
 //1/26/26
 //CSC-134
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    string item = "Percy Jackson & the Olympians: The Lightning Thief";
+    int item_count = 2010;
+    double price_each = 226.5;
+
+    cout << "The movie " << item << " ";
+    cout << "came out in " << item_count << " ";
+    cout << "and had a worldwide gross of around " << price_each << " million dollars" << endl;
+}
