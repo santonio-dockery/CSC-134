8b9452ef38f8d18f7d97ad921fc626e3f03efb03
santonio-dockery <dockery0605@gmail.com>
2026-01-27 04:54:07 +0000

    finished m1hw1

--- m1hw1_dockery.cpp
+++ m1hw1_dockery.cpp
@@ -12,7 +12,15 @@ int main() {
     int item_count = 2010;
     double price_each = 226.5;
 
-    cout << "The movie " << item << " ";
+cout << "The movie \"" << item << "\" ";
     cout << "came out in " << item_count << " ";
-    cout << "and had a worldwide gross of around " << price_each << " million dollars" << endl;
+    cout << "and had a worldwide gross of around " << price_each << " million dollars." << endl;
+
+    cout << "The movie follows a boy who learns he is the son of poseidon and that he must stop a war between the gods." << endl;
+
+   cout << "\"Your head is full of kelp.\""<< endl;
+   cout << "\"Deadlines just aren't real to me until I'm staring one in the face.\"" << endl;
+ cout << "\"If my life is going to mean anything, I have to live it myself.\""<< endl;
+cout << "This film is based on the novel by Rick Riordan." << endl;
+
 }
