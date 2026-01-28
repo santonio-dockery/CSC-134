a6291a4a2a51965f3b3f4218484da751d49d5fbd
santonio-dockery <dockery0605@gmail.com>
2026-01-28 18:38:21 +0000

    started m2lab1

---
+++ m2lab1_dockery.cpp
@@ -0,0 +1,36 @@
+/*
+CSC 134
+Santonio Dockery
+1/28/26
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+
+    // Part 1 -Set up variables
+    // Crate variables
+    double length, width, height; // in feet
+    // price variables
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+    double cost, customer_price, profit; // In $
+
+    // Part 2 - Input
+    cout << "Crate Purchase Program" << endl;
+    cout << "Enter the size of your crate \n";
+    cout << "Length?";
+    cin >> length;
+    cout << "Width?";
+    cin >> width;
+    cout << "Height?";
+    cin >> height;
+
+    //Part 3-Calculation
+
+    // Part 4 -Output
+
+    return 0;
+}
