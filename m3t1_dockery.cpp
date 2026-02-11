1cb3ed4ae88e9b8ad99ba57a2070bb9d8beb14e4
santonio-dockery <dockery0605@gmail.com>
2026-02-11 18:30:07 +0000

    started m3t1

---
+++ m3t1_dockery.cpp
@@ -0,0 +1,36 @@
+/*
+m3t1
+compare tow triangles
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+      // Program:measure two triangles
+      //declare variables
+      double length1, width1, area1;
+      double length2, width2, area2;
+
+      // get info from user
+      cout << "Please enter the measurements for two rectanges." << endl << endl;
+      cout << "Rectange 1" << endl;
+      cout << "Length?";
+      cin >> length1;
+      cout << "Width";
+      cin >> width1;
+      cout << endl;
+        cout << "Rectange 2" << endl;
+      cout << "Length?";
+      cin >> length2;
+      cout << "Width";
+      cin >> width2;
+
+      //calculate the areas
+      area1 = length1 * width1;
+      area2 = length2 * width2;
+
+      //print the output
+      cout << "Rectangle 1 is area: " << area1 << endl;
+      cout << "Rectangle 2 is area: " << area1 << endl;
+
+}
